abb58d9a8a479575779f543da2844d2c5111206c
Your Name <you@example.com>
2022-03-13 09:53:26 +0000

    errer

--- hrsystem01/settings.py
+++ hrsystem01/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    #'accounts'
+    'accounts'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-#AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 #SITE_ID = 1
 
 #AUTHENTICATION_BACKENDS = (
