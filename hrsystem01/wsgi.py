288413336cb55177d40cd7c086d973446aa39712
Your Name <you@example.com>
2022-03-13 09:10:56 +0000

    fairstcommit

---
+++ hrsystem01/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hrsystem01 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrsystem01.settings')
+
+application = get_wsgi_application()
